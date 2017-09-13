64d7ea3c2d456851ede2cf3f9a7fd53bbc34dfc2
朱良 <zhuliang@int-yt.com>
2017-09-13 22:05:47 +0800

    base页面弹出框的列表描述信息

--- frontend/src/components/base/base-item-expand.vue
+++ frontend/src/components/base/base-item-expand.vue
@@ -2,22 +2,14 @@
     .expand-container {
         font-size: 14px;
     }
-    .expand-row{
-        margin-bottom: 16px;
-    }
-    .expand-row .ivu-col:hover {
-        background: #e9eaec;
-    }
-    .no-data {
+    .desc {
         display: flex;
-        justify-content: center;
+        justify-content: flex-start;
     }
 </style>
 <template>
     <div class="expand-container">
-        <Row class="expand-row">
-            <p class="no-data">{{ row }}</p>
-        </Row>
+        <p class="desc">{{ row.desc }}</p>
     </div>
 </template>
 <script>
