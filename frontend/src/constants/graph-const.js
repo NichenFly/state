dbff2abfa4df75b870966276db84c8ebc8a0d7df
朱良 <zhuliang@int-yt.com>
2017-09-17 12:28:19 +0800

    Label-color

--- frontend/src/constants/graph-const.js
+++ frontend/src/constants/graph-const.js
@@ -6,4 +6,6 @@ export const ERROR_COLOR = '#f94854'
 
 export const SYMBOL_SIZE = 94
 
+export const LABEL_COLOR = '#000'
+
 export const COLORS = ['#8b0097', '#b6e095', '#003494', '#adc600', '#da7800', '#0098a6', '#b790d8', '#1b008d', '#f3bea2', '#be0046', '#95c9e0', '#f1a1a5', '#dabe00', '#149500', '#f3d7a2']
