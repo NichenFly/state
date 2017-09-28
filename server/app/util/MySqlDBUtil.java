2d845c1c8456cd798489eb23ee58437bdf688d34
朱良 <zhuliang@int-yt.com>
2017-09-28 20:37:17 +0800

    去掉短信相关

--- server/app/util/MySqlDBUtil.java
+++ server/app/util/MySqlDBUtil.java
@@ -40,7 +40,7 @@ public class MySqlDBUtil {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			String url = "jdbc:mysql://" + host + ":" + port + "/sys";
-			DriverManager.setLoginTimeout(1); // 10s连接不上即认为连接失败
+			DriverManager.setLoginTimeout(10); // 10s连接不上即认为连接失败
 			con = DriverManager.getConnection(url, user, passwd);
 		} catch (Exception e) {
 			Logger.error("无法连接服务器: %s", host);
