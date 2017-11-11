a88b2e2822ab92678bc30f94b50269e961e3e31a
朱良 <zhuliang@int-yt.com>
2017-11-11 18:40:00 +0800

    登录界面:回车事件

--- frontend/src/components/login.vue
+++ frontend/src/components/login.vue
@@ -10,16 +10,16 @@
                 </div> -->
                 <div>
                     <h2 class="title">超级监控系统</h2>
-                    <Form ref="formCustom" :model="formCustom" :rules="ruleCustom" :label-width="80">
+                    <Form ref="formLogin" :model="formLogin" :rules="ruleCustom" :label-width="80">
                         <FormItem label="用户名" prop="username">
-                            <Input type="text" v-model="formCustom.username" placeholder="请输入用户名"></Input>
+                            <Input type="text" v-model="formLogin.username" placeholder="请输入用户名"></Input>
                         </FormItem>
                         <FormItem label="密码" prop="passwordCheck">
-                            <Input type="password" v-model="formCustom.password"  @keyup.enter="handleReset('formCustom')" placeholder="请输入密码"></Input>
+                            <Input type="password" v-model="formLogin.password"  @keyup.enter.native="handleSubmit('formLogin')" placeholder="请输入密码"></Input>
                         </FormItem>
                         <FormItem>
-                            <Button type="primary" @click="handleSubmit('formCustom')">提交</Button>
-                            <Button type="ghost" @click="handleReset('formCustom')" style="margin-left: 8px">重置</Button>
+                            <Button type="primary" @click="handleSubmit('formLogin')">提交</Button>
+                            <Button type="ghost" @click="handleReset('formLogin')" style="margin-left: 8px">重置</Button>
                         </FormItem>
                     </Form>
                 </div>
@@ -36,43 +36,17 @@
 
     export default {
         data () {
-            const validateUsername = (rule, value, callback) => {
-                if (!value) {
-                    return callback(new Error('请输入用户名'))
-                } else {
-                    callback()
-                }
-                // 模拟异步验证效果
-                // setTimeout(() => {
-                //     if (!Number.isInteger(value)) {
-                //         callback(new Error('请输入数字值'))
-                //     } else {
-                //         if (value < 18) {
-                //             callback(new Error('必须年满18岁'))
-                //         } else {
-                //             callback()
-                //         }
-                //     }
-                // }, 1000)
-            }
-            const validatePassword = (rule, value, callback) => {
-                if (value === '') {
-                    callback(new Error('请输入密码'))
-                } else {
-                    callback()
-                }
-            }
             return {
-                formCustom: {
+                formLogin: {
                     username: '',
                     password: ''
                 },
                 ruleCustom: {
                     username: [
-                        { validator: validateUsername, trigger: 'blur' }
+                        { required: true, message: '用户名不允许为空', trigger: 'blur' }
                     ],
                     password: [
-                        { validator: validatePassword, trigger: 'blur' }
+                        { required: true, message: '密码不允许为空', trigger: 'blur' }
                     ]
                 }
             }
@@ -80,7 +54,10 @@
         methods: {
             handleSubmit (name) {
                 this.$refs[name].validate((valid) => {
-                    login(this.formCustom).then((res) => {
+                    if (!valid) {
+                        return
+                    }
+                    login(this.formLogin).then((res) => {
                         if (res.code === CODE_OK) {
                             this.$Notice.close(loginNoticeName)
                             let url = getOriginUrl()
